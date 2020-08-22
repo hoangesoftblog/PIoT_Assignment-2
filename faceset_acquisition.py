2a7eda3bbb7fe84b522f8a206e701084d03a6996
Hieu <s3695336@rmit.edu.vn>
2020-08-22 13:31:47 +0700

    Able to show video capture

---
+++ faceset_acquisition.py
@@ -0,0 +1,23 @@
+import cv2, time
+
+video = cv2.VideoCapture(0)
+
+while True:
+    # Create a frame object
+    check, frame = video.read()
+
+    print(check)
+    print(frame) #Represent image with matrices
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Capturing" , gray)
+
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+# Shut down camera
+video.release()
+
+cv2.destroyAllWindows
