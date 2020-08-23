8be03f38cf858a454c8d25f2596c3d4a19f52096
Hieu <s3695336@rmit.edu.vn>
2020-08-23 16:59:49 +0700

    Facial recognition function added

--- face_recognition.py
+++ face_recognition.py
@@ -128,8 +128,75 @@ def train_faceset(id):
     # Save the model into trainer.yml
     recognizer.save('trainer/trainer.yml')
 
+def face_recognition_start(id):
+    # Create Local Binary Patterns Histograms for face recognization
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+    # Load the trained mode
+    recognizer.read('trainer/trainer.yml')
+
+    # Load prebuilt model for Frontal Face
+    cascadePath = "haarcascade_frontalface_default.xml"
+
+    # Create classifier from prebuilt model
+    faceCascade = cv2.CascadeClassifier(cascadePath)
+    # Set the font style
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    # Initialize and start the video frame capture
+    cam = cv2.VideoCapture(0)
+
+    # Loop
+    while True:
+        # Read the video frame
+        ret, im =cam.read()
+
+        # Convert the captured frame into grayscale
+        gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+
+        # Get all face from the video frame
+        faces = faceCascade.detectMultiScale(gray, 1.2,5)
+
+        # For each face in faces
+        for(x,y,w,h) in faces:
+
+            # Create rectangle around the face
+            cv2.rectangle(im, (x-20,y-20), (x+w+20,y+h+20), (0,255,0), 4)
+
+            # Recognize the face belongs to which ID
+            Id = recognizer.predict(gray[y:y+h,x:x+w])
+
+            # Check the ID if exist 
+            if(Id[0] == 1):
+                Id = "Hieu"
+            #If not exist, then it is Unknown
+            elif(Id[0] == 2):
+                Id = "Uyen"
+            else:
+                print(Id)
+                Id = "Unknown"
+
+            # Put text describe who is in the picture
+            cv2.rectangle(im, (x-22,y-90), (x+w+22, y-22), (0,255,0), -1)
+            cv2.putText(im, str(Id), (x,y-40), font, 2, (255,255,255), 3)
+
+        # Display the video frame with the bounded rectangle
+        cv2.imshow('im',im) 
+
+        # If 'q' is pressed, close program
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    # Stop the camera
+    cam.release()
+
+    # Close all windows
+    cv2.destroyAllWindows()
+
+
 
 
 # show_video_capture()
-# faceset_capture('0001')
+# faceset_capture('0002')
 train_faceset('0001')
+face_recognition_start('0001')
