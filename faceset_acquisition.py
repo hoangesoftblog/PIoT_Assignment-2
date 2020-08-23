6c8ecaaab1d11815ab56d3c486bc93ebe7bcb031
Hieu <s3695336@rmit.edu.vn>
2020-08-24 00:58:22 +0700

    Added testing functionalities

--- faceset_acquisition.py
+++ faceset_acquisition.py
@@ -1,13 +1,27 @@
 import cv2, time, os
 import numpy as np
 from PIL import Image
+import json
+
+list_of_users = {}
+
+def write_user_dataset(id, name):
+    list_of_users.update({str(id):name})
+    js = json.dumps(list_of_users)
+    user_file = open("user_data.json","w")
+    user_file.write(js)
+    user_file.close
+
+def read_user_dataset():
+    with open("user_data.json") as user_file:
+        return json.load(user_file)
 
 # For demonstration purpose
 """For demonstration purpose 
 """
 
 def show_video_capture():
-    video = cv2.VideoCapture(1)
+    video = cv2.VideoCapture(0)
     video.set(3, 480)
     video.set(4, 480)
     
@@ -36,9 +50,10 @@ def show_video_capture():
 """When run will start capturing images of faces on the camera, save in the /user_dataset folder
 Naming scheme is based on id
 """
-def faceset_capture(id):
+def faceset_capture(id, name):
+
     # Start camera
-    camera = cv2.VideoCapture(1)
+    camera = cv2.VideoCapture(0)
 
     # Use casecade identifier to detect frontal faces
     face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -56,8 +71,8 @@ def faceset_capture(id):
         faces = face_detector.detectMultiScale(gray, 1.3, 5)
         for (x,y,w,h) in faces:
             cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0 ,0) , 2)
-            # count += 1
-            # cv2.imwrite("user_dataset/User."+str(id)+"."+str(count)+".jpg", gray[y:y+h,x:x+w])
+            count += 1
+            cv2.imwrite("user_dataset/User."+str(id)+"."+str(count)+".jpg", gray[y:y+h,x:x+w])
 
         # Display the  frame, with bounded rectangle on the person's face
         cv2.imshow('frame', frame)
@@ -65,6 +80,9 @@ def faceset_capture(id):
         # To stop, press 'q' for at least 100ms or 50 images are taken reach 50
         if (cv2.waitKey(100) & 0xFF == ord('q')) or count > 50:
             break
+
+    # list_of_users[str(id)] = name
+    write_user_dataset(str(id), name)
 
     # Ends camera
     camera.release()
@@ -118,19 +136,79 @@ def train_faceset(id):
     # Save the model into trainer.yml
     recognizer.save('trainer/trainer.yml')
 
+def face_recognition_start(id):
+    # Load users
+    list_of_users = read_user_dataset()
+    if str(id) not in list_of_users:
+        print("USER ID NOT FOUND! CANNOT FIND FACE")
+        return False
+
+    # Create Local Binary Patterns Histograms for face recognization
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+    # Load the trained mode
+    recognizer.read('trainer/trainer.yml')
+
+    # Load prebuilt model for Frontal Face
+    cascadePath = "haarcascade_frontalface_default.xml"
+
+    # Create classifier from prebuilt model
+    faceCascade = cv2.CascadeClassifier(cascadePath)
+    # Set the font style
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    # Initialize and start the video frame capture
+    cam = cv2.VideoCapture(0)
+
+    # Loop
+    while True:
+        # Read the video frame
+        ret, im =cam.read()
+
+        # Convert the captured frame into grayscale
+        gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+
+        # Get all face from the video frame
+        faces = faceCascade.detectMultiScale(gray, 1.2,5)
+
+        # For each face in faces
+        for(x,y,w,h) in faces:
+
+            # Create rectangle around the face
+            cv2.rectangle(im, (x-20,y-20), (x+w+20,y+h+20), (0,255,0), 4)
+
+            print(recognizer.predict(gray[y:y+h,x:x+w]))
+            # Recognize the face belongs to which ID
+            Id = recognizer.predict(gray[y:y+h,x:x+w])
+
+            # Check the ID if exist 
+            if(str(Id[0]) == str(int(id))):
+                print("USER "+ list_of_users[str(id)]   +  " WITH MATCHING ID FOUND")
+                return True
+            else:
+                print(Id)
+                Id = "Unknown"
+
+            # Put text describe who is in the picture
+            cv2.rectangle(im, (x-22,y-90), (x+w+22, y-22), (0,255,0), -1)
+            cv2.putText(im, str(Id), (x,y-40), font, 2, (255,255,255), 3)
+
+        # Display the video frame with the bounded rectangle
+        cv2.imshow('im',im) 
+
+        # If 'q' is pressed, close program
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    # Stop the camera
+    cam.release()
+
+    # Close all windows
+    cv2.destroyAllWindows()
+
 
 
 # show_video_capture()
-<<<<<<< HEAD:faceset_acquisition.py
-<<<<<<< HEAD:face_recognition.py
-# faceset_capture('0002')
+faceset_capture('0001', 'Hieu')
 train_faceset('0001')
-face_recognition_start('0001')
-=======
-# faceset_capture('0001')
-train_faceset('0001')
->>>>>>> parent of 8be03f3... Facial recognition function added:faceset_acquisition.py
-=======
-# faceset_capture('0001')
-train_faceset('0001')
->>>>>>> parent of 8be03f3... Facial recognition function added:face_recognition.py
+face_recognition_start('0002')
