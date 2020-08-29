423d7657b2116640c88e1348c3ed3011c684a049
Hieu <s3695336@rmit.edu.vn>
2020-08-29 20:41:53 +0700

    Update socket_communication.py

--- socket_communication.py
+++ socket_communication.py
@@ -26,9 +26,9 @@ def tcp_start_server():
     print("Done.")
 
 def tcp_client_send_message(message):
-    HOST = input("Enter IP address of server: ")
+    # HOST = input("Enter IP address of server: ")
 
-    # HOST = "127.0.0.1" # The server's hostname or IP address.
+    HOST = "127.0.0.1" # The server's hostname or IP address.
     PORT = 65000         # The port used by the server.
     ADDRESS = (HOST, PORT)
 
@@ -49,50 +49,52 @@ def tcp_client_send_message(message):
         print("Disconnecting from server.")
     print("Done.")
 
-def udp_start_server():
-    localIP     = "127.0.0.1"
-    localPort   = 20001
-    bufferSize  = 1024
+# def udp_start_server():
+#     localIP     = "127.0.0.1"
+#     localPort   = 20001
+#     bufferSize  = 1024
 
-    msgFromServer       = "Hello UDP Client"
-    bytesToSend         = str.encode(msgFromServer)
+#     msgFromServer       = "Hello UDP Client"
+#     bytesToSend         = str.encode(msgFromServer)
 
     
-    # Create a datagram socket
-    # communication domain in which the socket should be created. Some of address families are AF_INET (IP), AF_INET6 (IPv6), 
-    # AF_UNIX (local channel, similar to pipes), AF_ISO (ISO protocols), and AF_NS (Xerox Network Systems protocols).
-    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+#     # Create a datagram socket
+#     # communication domain in which the socket should be created. Some of address families are AF_INET (IP), AF_INET6 (IPv6), 
+#     # AF_UNIX (local channel, similar to pipes), AF_ISO (ISO protocols), and AF_NS (Xerox Network Systems protocols).
+#     UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-    # Bind to address and ip
-    UDPServerSocket.bind((localIP, localPort))
-    print("UDP server up and listening")
+#     # Bind to address and ip
+#     UDPServerSocket.bind((localIP, localPort))
+#     print("UDP server up and listening")
 
-    # Listen for incoming datagrams
-    while(True):
+#     # Listen for incoming datagrams
+#     while(True):
 
-        bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-        message = bytesAddressPair[0]
-        address = bytesAddressPair[1]
-        clientMsg = "Message from Client:{}".format(message)
-        clientIP  = "Client IP Address:{}".format(address)
+#         bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+#         message = bytesAddressPair[0]
+#         address = bytesAddressPair[1]
+#         clientMsg = "Message from Client:{}".format(message)
+#         clientIP  = "Client IP Address:{}".format(address)
         
-        print(clientMsg)
-        print(clientIP)
+#         print(clientMsg)
+#         print(clientIP)
 
-        # Sending a reply to client
-        UDPServerSocket.sendto(bytesToSend, address)
+#         # Sending a reply to client
+#         UDPServerSocket.sendto(bytesToSend, address)
 
-def udp_client_send_message(message):
-    bytesToSend         = str.encode(message)
-    serverAddressPort   = ("127.0.0.1", 20001)
-    bufferSize          = 1024
+# def udp_client_send_message(message):
+#     bytesToSend         = str.encode(message)
+#     serverAddressPort   = ("127.0.0.1", 20001)
+#     bufferSize          = 1024
 
-    # Create a UDP socket at client side
-    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+#     # Create a UDP socket at client side
+#     UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-    # Send to server using created UDP socket
-    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-    msg = "Message from Server {}".format(msgFromServer[0])
+#     # Send to server using created UDP socket
+#     UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+#     msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+#     msg = "Message from Server {}".format(msgFromServer[0])
 
-    print(msg)
+#     print(msg)
+
+tcp_client_send_message("Hey")
