688655868974a3c496fa9e47cfca2da59d93ee15
Hieu <s3695336@rmit.edu.vn>
2020-09-03 13:52:18 +0700

    Added qr_code, updated socket communication

---
+++ qr_code.py
@@ -0,0 +1,38 @@
+import cv2
+
+def start_reading_qr():
+    # initialize the camera
+    cap = cv2.VideoCapture(0)
+    # initialize the cv2 QRCode detector
+    detector = cv2.QRCodeDetector()
+    while True:
+        _, img = cap.read()
+        # detect and decode
+        data, bbox, _ = detector.detectAndDecode(img)
+        # check if there is a QRCode in the image
+        if bbox is not None:
+            # display the image with lines
+            for i in range(len(bbox)):
+                # draw all lines
+                cv2.line(img, tuple(bbox[i][0]), tuple(bbox[(i+1) % len(bbox)][0]), color=(255, 0, 0), thickness=2)
+            if data:
+                print("[+] QR Code detected, data:", data)
+                cap.release()
+                cv2.destroyAllWindows()
+                return data
+        # display the result
+        cv2.imshow("img", img)
+        if cv2.waitKey(1) == ord("q"):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+def generate_qr_code(content):
+    # example data
+    data = content
+    # output file name
+    filename = str(content) + "_qr.png"
+    # generate qr code
+    img = qrcode.make(data)
+    # save img to a file
+    img.save(filename)
--- socket_communication.py
+++ socket_communication.py
@@ -1,8 +1,10 @@
 import socket
 
 def tcp_start_server():
-    ADDRESS = ("", 65000)
-
+    ADDRESS = ("localhost", 65000)  
+    # Standard loopback interface address (localhost)
+    # Port to listen on (non-privileged ports are > 1023)
+    
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind(ADDRESS)
         s.listen()
@@ -13,11 +15,11 @@ def tcp_start_server():
             print("Connected to {}".format(addr))
 
             while True:
-                data = conn.recv(4096)
+                data = conn.recv(1024)
+                # data = conn.recv(4096)
                 if(not data):
                     break
-                print("Received {} bytes of data decoded to: '{}'".format(
-                    len(data), data.decode()))
+                print("Received {} bytes of data decoded to: '{}'".format(len(data), data.decode()))
                 print("Sending data back.")
                 conn.sendall(data)
             
@@ -28,7 +30,8 @@ def tcp_start_server():
 def tcp_client_send_message(message):
     # HOST = input("Enter IP address of server: ")
 
-    HOST = "127.0.0.1" # The server's hostname or IP address.
+    # HOST = "127.0.0.1" # The server's hostname or IP address.
+    HOST = "localhost" # The server's hostname or IP address.
     PORT = 65000         # The port used by the server.
     ADDRESS = (HOST, PORT)
 
@@ -42,59 +45,12 @@ def tcp_client_send_message(message):
                 break
             
             s.sendall(message.encode())
-            data = s.recv(4096)
+            # data = s.recv(4096)
+            data = s.recv(1024)
             print("Received {} bytes of data decoded to: '{}'".format(
                 len(data), data.decode()))
         
         print("Disconnecting from server.")
     print("Done.")
-
-# def udp_start_server():
-#     localIP     = "127.0.0.1"
-#     localPort   = 20001
-#     bufferSize  = 1024
-
-#     msgFromServer       = "Hello UDP Client"
-#     bytesToSend         = str.encode(msgFromServer)
-
-    
-#     # Create a datagram socket
-#     # communication domain in which the socket should be created. Some of address families are AF_INET (IP), AF_INET6 (IPv6), 
-#     # AF_UNIX (local channel, similar to pipes), AF_ISO (ISO protocols), and AF_NS (Xerox Network Systems protocols).
-#     UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-#     # Bind to address and ip
-#     UDPServerSocket.bind((localIP, localPort))
-#     print("UDP server up and listening")
-
-#     # Listen for incoming datagrams
-#     while(True):
-
-#         bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-#         message = bytesAddressPair[0]
-#         address = bytesAddressPair[1]
-#         clientMsg = "Message from Client:{}".format(message)
-#         clientIP  = "Client IP Address:{}".format(address)
-        
-#         print(clientMsg)
-#         print(clientIP)
-
-#         # Sending a reply to client
-#         UDPServerSocket.sendto(bytesToSend, address)
-
-# def udp_client_send_message(message):
-#     bytesToSend         = str.encode(message)
-#     serverAddressPort   = ("127.0.0.1", 20001)
-#     bufferSize          = 1024
-
-#     # Create a UDP socket at client side
-#     UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-#     # Send to server using created UDP socket
-#     UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-#     msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-#     msg = "Message from Server {}".format(msgFromServer[0])
-
-#     print(msg)
 
 tcp_client_send_message("Hey")
