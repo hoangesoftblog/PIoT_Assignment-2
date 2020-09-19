fe6ac8b7835a3d6273fdaba42b4313eae75cc202
duc ho <duc.hominh10899@gmail.com>
2020-09-19 16:42:04 +0700

    modify the qrcode

--- camera.py
+++ camera.py
@@ -1,5 +1,5 @@
 import cv2
-ds_factor=0.6
+
 
 class VideoCamera(object):
     def __init__(self):
@@ -8,7 +8,34 @@ class VideoCamera(object):
     def __del__(self):
         self.video.release()
     
+    # def get_frame(self):
+    #     success, image = self.video.read()
+    #     ret, jpeg = cv2.imencode('.jpg', image)
+    #     return jpeg.tobytes()
+
     def get_frame(self):
-        success, image = self.video.read()
-        ret, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tobytes()
+    # initialize the camera
+        cap = cv2.VideoCapture(0)
+    # initialize the cv2 QRCode detector
+        detector = cv2.QRCodeDetector()
+        while True:
+            _, img = cap.read()
+            # detect and decode
+            data, bbox, _ = detector.detectAndDecode(img)
+            # check if there is a QRCode in the image
+            if bbox is not None:
+                # display the image with lines
+                for i in range(len(bbox)):
+                    # draw all lines
+                    cv2.line(img, tuple(bbox[i][0]), tuple(bbox[(i+1) % len(bbox)][0]), color=(255, 0, 0), thickness=2)
+                if data:
+                    print("[+] QR Code detected, data:", data)
+                    cap.release()
+                    cv2.destroyAllWindows()
+                    return data
+            # display the result
+            cv2.imshow("img", img)
+            if cv2.waitKey(1) == ord("q"):
+                break
+        cap.release()
+        cv2.destroyAllWindows()
