b7633031bdfc0b90e803f163a29bc5efee742c98
ho minh duc <duc.hominh10899@gmail.com>
2020-09-19 18:24:59 +0700

    Fix qrcode

--- camera.py
+++ camera.py
@@ -14,28 +14,28 @@ class VideoCamera(object):
     #     return jpeg.tobytes()
 
     def get_frame(self):
-    # initialize the camera
-        cap = cv2.VideoCapture(0)
-    # initialize the cv2 QRCode detector
         detector = cv2.QRCodeDetector()
+
         while True:
-            _, img = cap.read()
-            # detect and decode
-            data, bbox, _ = detector.detectAndDecode(img)
-            # check if there is a QRCode in the image
+            success, image = self.video.read()
+            data, bbox, success = detector.detectAndDecode(image)
+
             if bbox is not None:
-                # display the image with lines
                 for i in range(len(bbox)):
-                    # draw all lines
-                    cv2.line(img, tuple(bbox[i][0]), tuple(bbox[(i+1) % len(bbox)][0]), color=(255, 0, 0), thickness=2)
+                    cv2.line(image, tuple(bbox[i][0]), tuple(bbox[(i+1) % len(bbox)][0]), color=(255, 0, 0), thickness=2)
+
                 if data:
                     print("[+] QR Code detected, data:", data)
-                    cap.release()
                     cv2.destroyAllWindows()
                     return data
-            # display the result
-            cv2.imshow("img", img)
+
+            cv2.imshow("img", image)
             if cv2.waitKey(1) == ord("q"):
                 break
-        cap.release()
+
+            
+        self.video.release()
         cv2.destroyAllWindows()
+
+            
+        
